fa8da5dc99b15e83dc0fda1ba5be297bc0c074c7
zt <a1571976198@outlook.com>
2021-01-01 21:40:24 +0800

    .

---
+++ app/src/main/java/com/rj1932/zt/dajiaxiaojia/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rj1932.zt.dajiaxiaojia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
