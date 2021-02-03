b3d392ccb01ffa06c738b931f1d1ab075b7c5ffd
zt <a1571976198@outlook.com>
2021-02-04 00:23:23 +0800

    设计注册布局

---
+++ app/src/main/java/com/rj1932/zt/dajiaxiaojia/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.rj1932.zt.dajiaxiaojia
+
+import android.app.Activity
+import android.os.Bundle
+
+class LoginActivity : Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
