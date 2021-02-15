586da25fe16ef3eb774c95e71c56e59942ff6f00
zt <a1571976198@outlook.com>
2021-02-15 18:37:14 +0800

    .

--- app/src/main/java/com/rj1932/zt/dajiaxiaojia/AdActivity.kt
+++ app/src/main/java/com/rj1932/zt/dajiaxiaojia/AdActivity.kt
@@ -4,14 +4,15 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
-import android.widget.TextView
 import java.util.*
 
-class WelcomeActivity : Activity() {
-    var passtime : TextView? = null
+class AdActivity : Activity() {
+
+    val jump = false
+    val jumpTime = 5
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_advertise)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
         startMainActivity()
     }
@@ -19,12 +20,13 @@ class WelcomeActivity : Activity() {
     private fun startMainActivity() {
         val delayTask: TimerTask = object : TimerTask() {
             override fun run() {
-                val mainIntent = Intent(this@WelcomeActivity, AdActivity::class.java)
+                val mainIntent = Intent(this@AdActivity, MainActivity::class.java)
                 startActivity(mainIntent)
                 finish()
             }
         }
         val timer = Timer()
-        timer.schedule(delayTask, 5000)
+        timer.schedule(delayTask, 2000)
     }
+
 }
